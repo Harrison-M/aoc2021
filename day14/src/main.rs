6acf65d16dcce63a9a4d2b4e9b7eb9fa2308edf7
Hal Massey <harrison.massey@gmail.com>
2021-12-14 18:50:28 -0600

    feat(day14): Add part 2 solution

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,29 +1,70 @@
 use std::{collections::HashMap, env, fs};
 
-fn parse_input(input: &str) -> (&str, HashMap<&str, &str>) {
+type Cache = HashMap<((char, char), u8), HashMap<char, usize>>;
+
+fn parse_input(input: &str) -> (&str, HashMap<(char, char), char>) {
     let sections: Vec<_> = input.split("\n\n").collect();
     let rules: HashMap<_, _> = sections[1]
         .lines()
         .map(|line| {
             let mut parts = line.split(" -> ");
-            (parts.next().unwrap(), parts.next().unwrap())
+            let mut pair = parts.next().unwrap().chars();
+            (
+                (pair.next().unwrap(), pair.next().unwrap()),
+                parts.next().unwrap().chars().next().unwrap(),
+            )
         })
         .collect();
     (sections[0], rules)
 }
 
-fn step(polymer: String, rules: &HashMap<&str, &str>) -> String {
+fn merge(target: &mut HashMap<char, usize>, source: &HashMap<char, usize>) {
+    for (c, count) in source.iter() {
+        target
+            .entry(*c)
+            .and_modify(|n| *n += *count)
+            .or_insert(*count);
+    }
+}
+
+fn added_characters(
+    pair: (char, char),
+    steps: u8,
+    rules: &HashMap<(char, char), char>,
+    cache: &mut Cache,
+) -> HashMap<char, usize> {
+    if let Some(result) = cache.get(&(pair, steps)) {
+        return result.clone();
+    }
+    let next = steps - 1;
+    let mut counts: HashMap<char, usize> = HashMap::new();
+    let maybe_insert = rules.get(&pair);
+    if let Some(insert) = maybe_insert {
+        counts.insert(*insert, 1);
+        if next != 0 {
+            merge(
+                &mut counts,
+                &added_characters((pair.0, *insert), next, rules, cache),
+            );
+            merge(
+                &mut counts,
+                &added_characters((*insert, pair.1), next, rules, cache),
+            );
+        }
+    }
+    cache.insert((pair, steps), counts.clone());
+    counts
+}
+
+fn step(polymer: String, rules: &HashMap<(char, char), char>) -> String {
     let chars: Vec<_> = polymer.chars().collect();
-    let pairs: Vec<_> = chars
-        .windows(2)
-        .map(|pair| format!("{}{}", pair[0], pair[1]))
-        .collect();
+    let pairs: Vec<_> = chars.windows(2).map(|pair| (pair[0], pair[1])).collect();
 
     let mut next = String::from("");
 
     for pair in pairs {
-        let insert = rules.get(&pair[..]);
-        let first = pair.chars().next().unwrap();
+        let insert = rules.get(&pair);
+        let first = pair.0;
         next = match insert {
             Some(addition) => format!("{}{}{}", next, first, addition),
             None => format!("{}{}", next, first),
@@ -33,7 +74,7 @@ fn step(polymer: String, rules: &HashMap<&str, &str>) -> String {
     format!("{}{}", next, chars.last().unwrap())
 }
 
-fn part1(init: &str, rules: &HashMap<&str, &str>) -> usize {
+fn part1(init: &str, rules: &HashMap<(char, char), char>) -> usize {
     let mut polymer = init.to_string();
 
     for _ in 0..10 {
@@ -43,6 +84,27 @@ fn part1(init: &str, rules: &HashMap<&str, &str>) -> usize {
     let mut counts: HashMap<char, usize> = HashMap::new();
     for element in polymer.chars() {
         counts.entry(element).and_modify(|c| *c += 1).or_insert(1);
+    }
+
+    let min = counts.values().min().unwrap();
+    let max = counts.values().max().unwrap();
+
+    max - min
+}
+
+fn part2(polymer: &str, rules: &HashMap<(char, char), char>) -> usize {
+    let chars: Vec<char> = polymer.chars().collect();
+    let mut counts = HashMap::new();
+
+    // Initial counts
+    for c in chars.iter() {
+        counts.entry(*c).and_modify(|n| *n += 1).or_insert(1);
+    }
+
+    let pairs: Vec<_> = chars.windows(2).map(|pair| (pair[0], pair[1])).collect();
+    let mut cache: Cache = HashMap::new();
+    for pair in pairs {
+        merge(&mut counts, &added_characters(pair, 40, rules, &mut cache))
     }
 
     let min = counts.values().min().unwrap();
@@ -80,5 +142,5 @@ fn main() {
     let (polymer, rules) = parse_input(&contents);
 
     println!("Part 1: {}", part1(polymer, &rules));
-    // println!("Part 2: {}", part2(polymer, &rules));
+    println!("Part 2: {}", part2(polymer, &rules));
 }
